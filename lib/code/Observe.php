3caaf37277cf0cd37241ce7775f532c6634d2ad1
刘柱 <476319748@qq.com>
2017-11-09 17:15:43 +0800

    添加策略模式

---
+++ lib/code/Observe.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * create by simengphp
+ * website www.simengphp.com
+ * QQ 476319748 (备注设计模式)
+ * Date: 2017/11/9
+ * Time: 16:02
+ */
+namespace lib\code;
+interface Observe
+{
+    
+}
