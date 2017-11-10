bbf7da8247c7ccfbc939fc9d06e00acbc9ce5168
刘柱 <476319748@qq.com>
2017-11-10 16:45:18 +0800

    增加原型设计模式

---
+++ lib/code/Prototype.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * create by simengphp
+ * website www.simengphp.com
+ * QQ 476319748 (备注设计模式)
+ * Date: 2017/11/10
+ * Time: 16:43
+ */
+namespace lib\code;
+class Prototype
+{
+    public function __construct()
+    {
+
+    }
+
+    public function test(){
+
+    }
+}
